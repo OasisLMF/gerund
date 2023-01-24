910e07c8d96c6a0a0d99d97a02cba8ccb7451d7b
maxwellflitton <maxwellflitton@gmail.com>
2023-01-24 15:37:20 +0000

    updating server

--- gerund/commands/terminal_command.py
+++ gerund/commands/terminal_command.py
@@ -112,9 +112,9 @@ class TerminalCommand:
         vars_command: Optional[str] = self._process_variables()
 
         if self.key is None:
-            command_prefix = "ssh -A -o StrictHostKeyChecking=no"
+            command_prefix = "ssh -A -o StrictHostKeyChecking=accept-new"
         else:
-            command_prefix = f"ssh -A -o StrictHostKeyChecking=no -i '{self.key}'"
+            command_prefix = f"ssh -A -o StrictHostKeyChecking=accept-new -i '{self.key}'"
 
         if self._remote is True:
             buffer.append(f"{command_prefix} {self.username}@{self.ip_address}")
