3dd314fcb26b22e563fab239c2c57d9b37878499
maxwellflitton <maxwellflitton@gmail.com>
2023-01-27 16:09:22 +0000

    adding key check passing

--- gerund/commands/terminal_command.py
+++ gerund/commands/terminal_command.py
@@ -110,13 +110,14 @@ class TerminalCommand:
         """
         buffer: List[str] = []
         vars_command: Optional[str] = self._process_variables()
+        # TODO => add verbose command option "-o LogLevel=DEBUG"
+        ssh_options: str = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
         if self.key is None:
-            command_prefix = "ssh -A -o StrictHostKeyChecking=no"
+            command_prefix = f"ssh -A {ssh_options}"
         else:
-            command_prefix = f"ssh -A -o StrictHostKeyChecking=no -i '{self.key}'"
+            command_prefix = f"ssh -A {ssh_options} -i '{self.key}'"
 
-        # print(f"\n\n\n{command_prefix}\n\n\n")
         if self._remote is True:
             buffer.append(f"{command_prefix} {self.username}@{self.ip_address}")
             buffer.append("'")
